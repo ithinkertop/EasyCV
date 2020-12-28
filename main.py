3d6cb953d31c8dcf2721a7e6765904d6727bc9d1
botbot <thinkertop@163.com>
2020-12-28 06:25:43 +0000

    add sequences section

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from __future__ import absolute_import
 
 from datasets.tools import Sequence
+
 if __name__ == '__main__':
 
     seq = Sequence()
+
     seq.check_sequences()
 
+    sequence = seq.get_sequence("COCO")['type']
+
+    print(sequence)
